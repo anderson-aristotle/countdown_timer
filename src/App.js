4d036e7e26cf4554bb9119a24a78012362e68813
rex@billion <anderson.aristotle@gmail.com>
2019-10-19 08:27:41 -0400

    create/added stopwatch class component to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+
+import Stopwatch from "./Stopwatch";
 import Countdown from "./Countdown";
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div className="Time Is Money">
-        <div className="timer-app">Countdown</div>
-        <Countdown />
+      <div className="App">
+        <div className="Time Is Money"></div>
+        <div className="Timers">
+          <Stopwatch />
+          <Countdown />
+        </div>
       </div>
     );
   }
